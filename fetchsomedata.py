7317c2b735de8579ed30305c66c6e644e5dad468
タカダユウジ <yuji.png@gmail.com>
2025-12-31 16:37:18 +0900

    Add Streamlit app to visualize Uber pickups data

---
+++ fetchsomedata.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+@st.cache_data
+def data_load(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+data_load_state = st.text('Loading data...')
+
+data = data_load(nrows=10000)
+
+data_load_state.text('Done! (using st.cache_data)')
+
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
+
+st.subheader('Number of pickups by hour')
+hist_values = np.histogram(
+    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
+
+hour_to_filter = st.slider('hour', 0, 23, 17)  # min: 0h, max: 23h, default: 17h
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+st.subheader(f'Map of all pickups as {hour_to_filter}:00')
+st.map(filtered_data)
